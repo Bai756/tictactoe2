d2475b05db4169c5cfbe6221989af7b85ca9087e
En Qiao <enqiao2009@gmail.com>
2025-03-13 12:37:57 -0400

    debug off

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ def play_human(stdscr):
         mini_row = row % 3
         mini_col = col % 3
 
-        stdscr.addstr(h-1, 0, f"Row: {row}, Col: {col}") # debugging
-        stdscr.addstr(h-3, 0, f"Mini Row: {mini_row}, Mini Col: {mini_col}")
-        stdscr.addstr(h-2, 0, f"Mini Board Row: {mini_idx_row}, Mini Board Col: {mini_idx_col}")
-        stdscr.addstr(h-4, 0, f"Win Board: {win_board[mini_idx_row * 3 + mini_idx_col]}")
-        stdscr.addstr(h-5, 0, f"Current Player: {player}")
-        stdscr.addstr(h-6, 0, f"Current Board: {current_board}")
+        # stdscr.addstr(h-1, 0, f"Row: {row}, Col: {col}") # debugging
+        # stdscr.addstr(h-3, 0, f"Mini Row: {mini_row}, Mini Col: {mini_col}")
+        # stdscr.addstr(h-2, 0, f"Mini Board Row: {mini_idx_row}, Mini Board Col: {mini_idx_col}")
+        # stdscr.addstr(h-4, 0, f"Win Board: {win_board[mini_idx_row * 3 + mini_idx_col]}")
+        # stdscr.addstr(h-5, 0, f"Current Player: {player}")
+        # stdscr.addstr(h-6, 0, f"Current Board: {current_board}")
 
         print_board(stdscr, current_pos, big_board)
         key = stdscr.getch()
